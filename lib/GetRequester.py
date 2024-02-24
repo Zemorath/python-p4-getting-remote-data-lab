4093e301a04ee98d306a3cbb532da660fb4e9198
Zemorath <trentward10@gmail.com>
2024-02-24 13:29:22 -0600

    Getting Remote Lab Complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        json_list = []
+        json_response = json.loads(self.get_response_body())
+        for response in json_response:
+            json_list.append(response)
+        
+        return json_list
